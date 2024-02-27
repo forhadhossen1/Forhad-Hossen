f39230708c847817bf61b3754c793a0b7387285e
forhadhossen1 <mr.forhad1030@gamil.com>
2024-02-27 15:55:56 +0600

    create heros two section

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -1,11 +1,20 @@
 import herobg from '../assets/herobg.png';
-
+import heroman from '../assets/heroman.png';
 const Hero = () => {
     return (
-        <div className='bg-cover bg-center h-[120vh]' style={{ backgroundImage: `url(${herobg})` }}>
+        <div className='bg-cover bg-center h-[100vh]' style={{ backgroundImage: `url(${herobg})` }}>
             <div className='max-w-screen-xl mx-auto pt-16'>
 
-                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quis cumque mollitia numquam, in et temporibus at facilis exercitationem labore adipisci quaerat quae autem, aperiam dicta beatae, assumenda voluptatibus minima eligendi!</p>
+                <div className='min-h-[100vh] flex flex-col lg:flex-row justify-between items-center p-3'>
+                    <div>
+                        <h2 className='text-3xl md:text-5xl font-bold tracking-wide hover:tracking-widest'>Hi _ <br /> <span className=' text-transparent bg-clip-text bg-gradient-to-r from-pink-600 via-indigo-500 to-pink-500'>I am Forhad </span></h2>
+                        <h1 className='text-5xl md:text-7xl font-bold tracking-wide py-5'>Web <span className='text-transparent bg-clip-text bg-gradient-to-r from-pink-600 via-indigo-500 to-pink-500'>Developer</span></h1>
+                    </div>
+
+                    <div>
+                        <img src={heroman} alt="" />
+                    </div>
+                </div>
 
             </div>
         </div>
