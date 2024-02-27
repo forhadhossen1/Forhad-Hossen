67bf8096b5db923c49fe2d01a6ade23b239ac99c
forhadhossen1 <mr.forhad1030@gamil.com>
2024-02-27 17:12:02 +0600

    added short story in hero section

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -6,12 +6,14 @@ const Hero = () => {
             <div className='max-w-screen-xl mx-auto pt-16'>
 
                 <div className='min-h-[100vh] flex flex-col lg:flex-row justify-between items-center p-3'>
-                    <div>
+                    <div className='flex-1'>
                         <h2 className='text-3xl md:text-5xl font-bold tracking-wide hover:tracking-widest'>Hi _ <br /> <span className=' text-transparent bg-clip-text bg-gradient-to-r from-pink-600 via-indigo-500 to-pink-500'>I am Forhad </span></h2>
                         <h1 className='text-5xl md:text-7xl font-bold tracking-wide py-5'>Web <span className='text-transparent bg-clip-text bg-gradient-to-r from-pink-600 via-indigo-500 to-pink-500'>Developer</span></h1>
+
+                        <p className='font-medium text-xl text-gray-100 w-full md:w-2/3'>Welcome to my web corner! I am Forhad , a profasional MERN (MongoDB, Express.js, React, Node.js) stack developer. With a love for crafting seamless and dynamic web applications, I thrive on turning innovative ideas into reality. My expertise lies in leveraging the power of MongoDB for robust data storage, Express.js for scalable server-side development, React for building intuitive user interfaces,....<button className="btn btn-link text-blue-500"> Read More</button></p>
                     </div>
 
-                    <div>
+                    <div className=''>
                         <img src={heroman} alt="" />
                     </div>
                 </div>
