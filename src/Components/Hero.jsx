4ad6e9cfe9a3ad87a3f6155971a745cad60805c6
forhadhossen1 <mr.forhad1030@gamil.com>
2024-02-27 18:20:37 +0600

    create animaion motion

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -3,7 +3,7 @@ import heroman from '../assets/heroman.png';
 import { motion } from "framer-motion"
 
 const textVariants = {
-    initial : {
+    initial: {
         x: -500,
         opacity: 0,
     },
@@ -12,10 +12,32 @@ const textVariants = {
         opacity: 1,
         transition: {
             duration: 1,
-            staggerChildren: 0.1,   
+            staggerChildren: 0.1,
         },
+    },
+    scrollButton: {
+        opacity: 0,
+        y: 10,
+        transition: {
+            duration: 2,
+            repeat: Infinity,
+        }
     }
-}
+};
+
+const sliderVariants = {
+    initial: {
+        x: 0,
+    },
+    animate: {
+        x: "-220%",
+        transition: {
+            repeat: Infinity,
+            repeatType: "mirror",
+            duration: 20,
+        },
+    },
+};
 
 const Hero = () => {
     return (
@@ -23,16 +45,16 @@ const Hero = () => {
             <div className='max-w-screen-xl mx-auto pt-16'>
 
                 <div className='min-h-[100vh] flex flex-col lg:flex-row justify-between items-center p-3'>
+                    
+                    <motion.div variants={sliderVariants} initial="initial" animate="animate" className='font-extrabold text-[70vh] text-nowrap absolute bottom-[-90px] opacity-5 text-gray-200 w-1/2'>
+                        Professional Web Developer
+                    </motion.div>
 
-                    <div>
-                        <h2 className='font-extrabold text-[40vh] absolute bottom-32 text-nowrap opacity-5'>Professional Web Developer</h2>
-                    </div>
+                    <motion.div className='flex-1' variants={textVariants} initial="initial" animate="animate">
+                        <motion.h2 variants={textVariants} className='text-3xl md:text-5xl font-bold tracking-wide hover:tracking-widest'>Hi _ <br /> <span className=' text-transparent bg-clip-text bg-gradient-to-r from-pink-600 via-indigo-500 to-pink-500'>I am Forhad </span></motion.h2>
+                        <motion.h1 variants={textVariants} className='text-5xl md:text-7xl font-bold tracking-wide py-5'>Web <span className='text-transparent bg-clip-text bg-gradient-to-r from-pink-600 via-indigo-500 to-pink-500'>Developer</span></motion.h1>
 
-                    <motion.div className='flex-1' variants = {textVariants} initial="initial" animate="animate">
-                        <motion.h2 variants = {textVariants} className='text-3xl md:text-5xl font-bold tracking-wide hover:tracking-widest'>Hi _ <br /> <span className=' text-transparent bg-clip-text bg-gradient-to-r from-pink-600 via-indigo-500 to-pink-500'>I am Forhad </span></motion.h2>
-                        <motion.h1 variants = {textVariants} className='text-5xl md:text-7xl font-bold tracking-wide py-5'>Web <span className='text-transparent bg-clip-text bg-gradient-to-r from-pink-600 via-indigo-500 to-pink-500'>Developer</span></motion.h1>
-
-                        <motion.p variants = {textVariants} className='font-medium text-xl text-gray-100 w-full md:w-2/3'>Welcome to my web corner! I am Forhad , a Professional MERN (MongoDB, Express.js, React, Node.js) stack developer. With a love for crafting seamless and dynamic web applications, I thrive on turning innovative ideas into reality. My expertise lies in leveraging the power of MongoDB for robust data storage, Express.js for scalable server-side development, React for building intuitive user interfaces,....<button className="btn btn-link text-blue-500"> Read More</button></motion.p>
+                        <motion.p variants={textVariants} className='font-medium text-xl text-gray-100 w-full md:w-2/3'>Welcome to my web corner! I am Forhad , a Professional MERN (MongoDB, Express.js, React, Node.js) stack developer. With a love for crafting seamless and dynamic web applications, I thrive on turning innovative ideas into reality. My expertise lies in leveraging the power of MongoDB for robust data storage, Express.js for scalable server-side development, React for building intuitive user interfaces,....<button className="btn btn-link text-blue-500"> Read More</button></motion.p>
                     </motion.div>
 
                     <div className=''>
