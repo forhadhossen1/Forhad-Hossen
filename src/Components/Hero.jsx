0d1974eb59d85f2f2bd2cc978db969ebeaf52411
forhadhossen1 <mr.forhad1030@gamil.com>
2024-02-27 23:09:36 +0600

    added some style in banner

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -30,7 +30,7 @@ const sliderVariants = {
         x: 0,
     },
     animate: {
-        x: "-220%",
+        x: "-100%",
         transition: {
             repeat: Infinity,
             repeatType: "mirror",
@@ -45,10 +45,8 @@ const Hero = () => {
             <div className='max-w-screen-xl mx-auto pt-16'>
 
                 <div className='min-h-[100vh] flex flex-col lg:flex-row justify-between items-center p-3'>
-                    
-                    <motion.div variants={sliderVariants} initial="initial" animate="animate" className='font-extrabold text-[70vh] text-nowrap absolute bottom-[-90px] opacity-5 text-gray-200 w-1/2'>
-                        Professional Web Developer
-                    </motion.div>
+
+
 
                     <motion.div className='flex-1' variants={textVariants} initial="initial" animate="animate">
                         <motion.h2 variants={textVariants} className='text-3xl md:text-5xl font-bold tracking-wide hover:tracking-widest'>Hi _ <br /> <span className=' text-transparent bg-clip-text bg-gradient-to-r from-pink-600 via-indigo-500 to-pink-500'>I am Forhad </span></motion.h2>
@@ -57,9 +55,18 @@ const Hero = () => {
                         <motion.p variants={textVariants} className='font-medium text-xl text-gray-100 w-full md:w-2/3'>Welcome to my web corner! I am Forhad , a Professional MERN (MongoDB, Express.js, React, Node.js) stack developer. With a love for crafting seamless and dynamic web applications, I thrive on turning innovative ideas into reality. My expertise lies in leveraging the power of MongoDB for robust data storage, Express.js for scalable server-side development, React for building intuitive user interfaces,....<button className="btn btn-link text-blue-500"> Read More</button></motion.p>
                     </motion.div>
 
-                    <div className=''>
+                    <div className='flex'>
+                        <div className='flex flex-col justify-center items-center mt-5'>
+                            <div className='w-5 h-5 rounded-full bg-violet-600'></div>
+                            <div className='w-1 sm:h-80 h-40 bg-gradient-to-b from-violet-500'>
+                            </div>
+                        </div>
                         <img src={heroman} alt="" />
                     </div>
+
+                    <motion.div variants={sliderVariants} initial="initial" animate="animate" className='font-extrabold text-4xl md:text-7xl lg:text-[20vh] text-nowrap absolute bottom-0 opacity-5 text-gray-200 w-full md:w-1/2'>
+                        Web Developer
+                    </motion.div>
                 </div>
 
             </div>
