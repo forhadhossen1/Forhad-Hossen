00924ef9b3140e9d568f9e5feaf4dad73ccde36d
forhadhossen1 <mr.forhad1030@gmail.com>
2024-03-14 00:48:14 +0600

    change heading color

--- src/Components/MyProject.jsx
+++ src/Components/MyProject.jsx
@@ -6,7 +6,7 @@ const MyProject = () => {
     const [projects] = useProject();
     return (
         <div className="max-w-screen-xl mx-auto">
-            <h1 className="text-4xl md:text-6xl text-center font-bold text-transparent bg-clip-text bg-gradient-to-r from-purple-600 to-pink-500 py-8">My Project</h1>
+            <h1 className="text-4xl md:text-6xl text-center font-bold text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-pink-500">My Project</h1>
 
             <div>
                 {
