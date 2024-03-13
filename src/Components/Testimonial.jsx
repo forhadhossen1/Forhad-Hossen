30a65f194a0f1e5c9a4849b5c5ff5d6d3a585f9e
forhadhossen1 <mr.forhad1030@gmail.com>
2024-03-14 03:58:02 +0600

    slider its very deficult and data map system uncommon

--- src/Components/Testimonial.jsx
+++ src/Components/Testimonial.jsx
@@ -1,19 +1,40 @@
 import useTestimonial from "../Hooks/useTestimonial";
+import TestimonialCard from "./TestimonialCard";
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import { Navigation } from 'swiper/modules';
 
 const Testimonial = () => {
     const [testimonials] = useTestimonial();
     return (
-        <div>
-            {
-                testimonials.map(testimonial => (
-                    <Testimonial
-                        key={testimonial._id}
-                        testimonial={testimonial}
-                    >
+        <div className="my-12 mt-16">
+            <div>
+                <h1 className="text-4xl font-bold text-center border-b-4 max-w-[250px] p-3 mx-auto">Testimonial</h1>
+            </div>
 
-                    </Testimonial>
-                ))
-            }
+            <Swiper navigation={true} modules={[Navigation]} className="mySwiper">
+
+                {
+                    testimonials.map(testimonial => (
+                        <SwiperSlide
+                            key={testimonial._id}
+                            testimonial={testimonial}
+                        >
+                            <div>
+                                hello world
+                            </div>
+                        </SwiperSlide>
+                    ))
+                }
+            </Swiper>
+
+
+
+
+
+
         </div>
     );
 };
