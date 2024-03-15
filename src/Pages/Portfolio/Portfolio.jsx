e3f643f8ff0868b46af54b73f7d5d4a01fc1c54b
forhadhossen1 <mr.forhad1030@gmail.com>
2024-03-15 16:14:14 +0600

    all setup portfolio page

--- src/Pages/Portfolio/Portfolio.jsx
+++ src/Pages/Portfolio/Portfolio.jsx
@@ -1,32 +1,27 @@
 import ProjectCard from "../../Components/ProjectCard";
 import useProject from "../../Hooks/useProject";
+import Footer from '../../Components/Footer';
 
 const Portfolio = () => {
     const [projects] = useProject();
     return (
-        <div className="py-16 max-w-screen-xl mx-auto">
-            <h1 className="text-4xl md:text-6xl text-center font-bold py-8 text-transparent bg-clip-text bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500">Portfolio</h1>
+        <div className=" max-w-screen-xl mx-auto">
+            <div className="py-16">
+                <h1 className="text-4xl md:text-6xl text-center font-bold py-8 text-transparent bg-clip-text bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500">Portfolio</h1>
 
-            {
-                projects.map(project => (
-                    <ProjectCard
-                        key={project._id}
-                        project={project}
-                    >
-                    </ProjectCard>
-                ))
-            }
+                {
+                    projects.map(project => (
+                        <ProjectCard
+                            key={project._id}
+                            project={project}
+                        >
+                        </ProjectCard>
+                    ))
+                }
 
-            <div className="max-w-screen-xl mx-auto my-12">
-
-                <div className="flex justify-between flex-col md:flex-row py-12">
-                    <div className="flex-1">
-                        <h1 className="text-4xl md:text-6xl font-bold p-3">Project Workflow</h1>
-                    </div>
-                    <div className="flex-1 p-3">
-                        <p>To commence the project, I initiate a comprehensive meeting with the client to gain a thorough understanding of their portfolio requirements and overarching goals. Following this, I delve into research, exploring portfolios of a similar nature to gather valuable insights and inspiration. With these insights in mind, I meticulously craft a layout that aligns with the structure envisioned for the client is portfolio. The concluding stage involves the design and development of the user interface, a process that commences once the chosen layout is finalized. This approach ensures a seamless progression from client collaboration and research to the thoughtful creation of a tailored and visually appealing user interface.</p>
-                    </div>
-                </div>
+            </div>
+            <div className="mt-10">
+                <Footer></Footer>
             </div>
         </div>
     );
