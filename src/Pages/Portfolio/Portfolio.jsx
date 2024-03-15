2dd7df87b108fd26c6e0334adfff9dd7a968d228
forhadhossen1 <mr.forhad1030@gmail.com>
2024-03-15 14:26:53 +0600

    create portfolio page

--- src/Pages/Portfolio/Portfolio.jsx
+++ src/Pages/Portfolio/Portfolio.jsx
@@ -1,8 +1,21 @@
+import ProjectCard from "../../Components/ProjectCard";
+import useProject from "../../Hooks/useProject";
 
 const Portfolio = () => {
+    const [projects] = useProject();
     return (
         <div className="py-16 max-w-screen-xl mx-auto">
-            To commence the project, I initiate a comprehensive meeting with the client to gain a thorough understanding of their portfolio requirements and overarching goals. Following this, I delve into research, exploring portfolios of a similar nature to gather valuable insights and inspiration. With these insights in mind, I meticulously craft a layout that aligns with the structure envisioned for the client is portfolio. The concluding stage involves the design and development of the user interface, a process that commences once the chosen layout is finalized. This approach ensures a seamless progression from client collaboration and research to the thoughtful creation of a tailored and visually appealing user interface.
+            <h1 className="text-4xl md:text-6xl text-center font-bold py-8 text-transparent bg-clip-text bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500">Portfolio</h1>
+
+            {
+                projects.map(project => (
+                    <ProjectCard
+                        key={project._id}
+                        project={project}
+                    >
+                    </ProjectCard>
+                ))
+            }
 
             <div className="max-w-screen-xl mx-auto my-12">
 
