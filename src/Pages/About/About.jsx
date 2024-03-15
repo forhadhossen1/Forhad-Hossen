689d0dbaeec968d921c27f1b61db7555f61fc648
forhadhossen1 <mr.forhad1030@gmail.com>
2024-03-15 16:16:55 +0600

    added about section item

--- src/Pages/About/About.jsx
+++ src/Pages/About/About.jsx
@@ -1,10 +1,24 @@
-
+import aboutImage from "../../assets/heroman.png"
 const About = () => {
     return (
         <div className="py-16 max-w-screen-xl mx-auto">
-            <p className="p-3">
-            First I hold a meeting with the client to understand his requirements and ultimate goals for his portfolio. After that I research that type of portfolio and get ideas. And create a layout according to his portfolio structure. At the final stage, the UI is designed and developed once its layout is chosen
-            </p>
+            <div>
+                <div>
+                    <img src={aboutImage} alt="forhad image" />
+                </div>
+
+                <div>
+                    <p>Hello, my name is Farhad. My passion for computer technology started at a young age and it has always brought me joy. As I grew up, I realized my true desire to immerse myself in the IT field, driven by its dynamic nature and the urge to stay updated with the latest advancements.
+
+                        The more I delve into this field, the more inspired I become, fueling my ambition to deepen my programming skills. I consider myself open-minded and determined, always eager to learn and adapt quickly to new challenges.
+
+                        Outside of my professional interests, I enjoy hobbies like painting, traveling, playing and being active. These experiences contribute to my specific perspective and problem-solving skills.
+
+                        I strongly believe that working with your team will not only allow me to contribute something new and valuable but also enable me to apply my knowledge to enhance the company is potential. Moreover, I am committed to fostering a productive and friendly environment, as I bring positivity and motivation to any team I join.
+
+                        I am a dedicated team player interested in making a meaningful contribution to your company while continuously growing and learning in the process.</p>
+                </div>
+            </div>
         </div>
     );
 };
