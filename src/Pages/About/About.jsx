4cac263c986f3a11e54f9ce0702123f8d6a757d5
forhadhossen1 <mr.forhad1030@gmail.com>
2024-03-16 01:22:27 +0600

    about p storymodify

--- src/Pages/About/About.jsx
+++ src/Pages/About/About.jsx
@@ -2,13 +2,14 @@ import aboutImage from "../../assets/heroman.png"
 const About = () => {
     return (
         <div className="py-16 max-w-screen-xl mx-auto">
-            <div>
-                <div>
+            <div className="flex flex-col md:flex-row justify-between items-center gap-4 my-12 p-3">
+                <div className="flex-1">
+                    <h1 className="text-4xl font-bold py-7">My Story....</h1>
                     <img src={aboutImage} alt="forhad image" />
                 </div>
 
-                <div>
-                    <p>Hello, my name is Farhad. My passion for computer technology started at a young age and it has always brought me joy. As I grew up, I realized my true desire to immerse myself in the IT field, driven by its dynamic nature and the urge to stay updated with the latest advancements.
+                <div className="flex-1">
+                    <p className="text-lg lg:text-xl">Hello, my name is Farhad. My passion for computer technology started at a young age and it has always brought me joy. As I grew up, I realized my true desire to immerse myself in the IT field, driven by its dynamic nature and the urge to stay updated with the latest advancements.
 
                         The more I delve into this field, the more inspired I become, fueling my ambition to deepen my programming skills. I consider myself open-minded and determined, always eager to learn and adapt quickly to new challenges.
 
