ceb7d65297a35678766156e2af0daaf0a3a14bcf
forhadhossen1 <mr.forhad1030@gmail.com>
2024-03-18 22:19:32 +0600

    added contact from

--- src/Pages/Contact/Contact.jsx
+++ src/Pages/Contact/Contact.jsx
@@ -2,35 +2,65 @@
 import emailjs from '@emailjs/browser';
 import { useRef } from 'react';
 
-export const Contact = () => {
-  const form = useRef();
+const Contact = () => {
+    const form = useRef();
 
-  const sendEmail = (e) => {
-    e.preventDefault();
+    const sendEmail = (e) => {
+        e.preventDefault();
 
-    emailjs
-      .sendForm('YOUR_SERVICE_ID', 'YOUR_TEMPLATE_ID', form.current, {
-        publicKey: 'YOUR_PUBLIC_KEY',
-      })
-      .then(
-        () => {
-          console.log('SUCCESS!');
-        },
-        (error) => {
-          console.log('FAILED...', error.text);
-        },
-      );
-  };
+        emailjs
+            .sendForm('YOUR_SERVICE_ID', 'YOUR_TEMPLATE_ID', form.current, {
+                publicKey: 'YOUR_PUBLIC_KEY',
+            })
+            .then(
+                () => {
+                    console.log('SUCCESS!');
+                },
+                (error) => {
+                    console.log('FAILED...', error.text);
+                },
+            );
+    };
 
-  return (
-    <form ref={form} onSubmit={sendEmail}>
-      <label>Name</label>
-      <input type="text" name="user_name" />
-      <label>Email</label>
-      <input type="email" name="user_email" />
-      <label>Message</label>
-      <textarea name="message" />
-      <input type="submit" value="Send" />
-    </form>
-  );
+    return (
+        <div className='max-w-screen-xl mx-auto'>
+            {/* <form ref={form} onSubmit={sendEmail}>
+                <label>Name</label>
+                <input type="text" name="user_name" />
+                <label>Email</label>
+                <input type="email" name="user_email" />
+                <label>Message</label>
+                <textarea name="message" />
+                <input type="submit" value="Send" />
+            </form> */}
+
+
+            <div className="grid max-w-screen-xl grid-cols-1 gap-8 px-8 py-16 mx-auto rounded-lg md:grid-cols-2 md:px-12 lg:px-16 xl:px-32 dark:bg-gray-800 dark:text-gray-100">
+                <div className="flex flex-col justify-between">
+                    <div className="space-y-2">
+                        <h2 className="text-4xl font-bold leading-tight lg:text-5xl">Let's talk!</h2>
+                        <div className="dark:text-gray-400">Vivamus in nisl metus? Phasellus.</div>
+                    </div>
+                    <img src="assets/svg/doodle.svg" alt="" className="p-6 h-52 md:h-64" />
+                </div>
+                <form className="space-y-6">
+                    <div>
+                        <label  className="text-sm">Full name</label>
+                        <input id="name" type="text" placeholder="" className="w-full p-3 rounded dark:bg-gray-800" />
+                    </div>
+                    <div>
+                        <label  className="text-sm">Email</label>
+                        <input id="email" type="email" className="w-full p-3 rounded dark:bg-gray-800" />
+                    </div>
+                    <div>
+                        <label  className="text-sm">Message</label>
+                        <textarea id="message" rows="3" className="w-full p-3 rounded dark:bg-gray-800"></textarea>
+                    </div>
+                    <button type="submit" className="w-full p-3 text-sm font-bold tracking-wide uppercase rounded dark:bg-violet-400 dark:text-gray-900">Send Message</button>
+                </form>
+            </div>
+        </div>
+    );
 };
+
+export default Contact
