77a40206ec8c48fb5bf9c217f601f41d22cd151a
forhadhossen1 <mr.forhad1030@gmail.com>
2024-03-22 03:39:37 +0600

    create manage project apge

---
+++ src/Pages/Dashboard/ManageProject.jsx
@@ -0,0 +1,10 @@
+
+const ManageProject = () => {
+    return (
+        <div>
+            yesh Manage project page Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum eaque voluptas pariatur eius corrupti, neque ipsa eligendi sint architecto ducimus natus deserunt doloribus in aliquid repudiandae voluptatem libero accusantium aperiam?
+        </div>
+    );
+};
+
+export default ManageProject;
