6855d45520c24e72268d9db037f58e0a6a701127
forhadhossen1 <mr.forhad1030@gmail.com>
2024-03-22 00:25:31 +0600

    fetch project data in backend local host

--- src/Hooks/useProject.jsx
+++ src/Hooks/useProject.jsx
@@ -3,9 +3,10 @@ import { useState } from "react";
 
 const useProject = () => {
     const [projects, setProjects] = useState([]);
-const [loading, setLoading] = useState([true])
+    const [loading, setLoading] = useState([true])
     useEffect(() => {
-        fetch('project.json')
+        // fetch('project.json')
+        fetch('http://localhost:5000/project')
             .then(res => res.json())
             .then(data => {
                 setProjects(data);
