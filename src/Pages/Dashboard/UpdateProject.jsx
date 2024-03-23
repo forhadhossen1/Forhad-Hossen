826c0eb120ab5fd5745c391ee5bca42b9fb34c86
forhadhossen1 <mr.forhad1030@gmail.com>
2024-03-24 01:57:40 +0600

    update system full ok

--- src/Pages/Dashboard/UpdateProject.jsx
+++ src/Pages/Dashboard/UpdateProject.jsx
@@ -8,6 +8,7 @@ const image_hosting_api = `https://api.imgbb.com/1/upload?key=${image_hosting_ke
 const UpdateProject = () => {
     const { register, handleSubmit } = useForm()
     const axiosPublic = useAxiosPublic();
+
     const { ProjectName, ProjectDescription, GitHubLink, LiveLink, _id } = useLoaderData();
 
     console.log(ProjectDescription, ProjectName)
@@ -86,7 +87,7 @@ const UpdateProject = () => {
                     </div>
 
                     <div>
-                        <input  type="file" {...register("image", { required: true })} className="file-input file-input-bordered file-input-secondary w-full max-w-xs" />
+                        <input  type="file"  {...register("image", { required: true })} className="file-input file-input-bordered file-input-secondary w-full max-w-xs" />
                     </div>
 
                     {/* <input  className="btn btn-block" type="submit" /> */}
