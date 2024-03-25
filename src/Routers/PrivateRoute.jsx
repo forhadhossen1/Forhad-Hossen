a6df2774f6fa0056c19fa7d9568c53a5a8dc4680
forhadhossen1 <mr.forhad1030@gmail.com>
2024-03-25 11:22:31 +0600

    create password secure system

--- src/Routers/PrivateRoute.jsx
+++ src/Routers/PrivateRoute.jsx
@@ -1,8 +1,7 @@
 
 import { useState } from 'react';
-import { Navigate } from 'react-router-dom';
 
-const adminPassword = 'admin420';
+const adminPassword = import.meta.env.VITE_ADMIN_LOGIN_KEY;
 
 const PrivateRoute = ({ children }) => {
     const [password, setPassword] = useState('');
@@ -30,9 +29,9 @@ const PrivateRoute = ({ children }) => {
                 <h2 className='text-xl font-bold pb-5'>Please enter the password to access the dashboard:</h2>
                 <div className='flex flex-col gap-5'>
                     <input className='text-black rounded-md p-2' type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
-                    <button className='text-xl bg-gray-700' onClick={handleLogin}>Login</button>
+                    <button className='btn btn-outline btn-secondary text-xl' onClick={handleLogin}>Login</button>
                 </div>
-                {loginAttempted && !authenticated && <p>Invalid password. Please try again.</p>}
+                {loginAttempted && !authenticated && <p className='py-3'>Invalid password. Please try again.</p>}
             </div>
         </div>
     );
