da4c8acccc303c09b51c36a73380d0a24137737f
forhadhossen1 <mr.forhad1030@gmail.com>
2024-03-25 15:22:42 +0600

    create portfolio heading

--- src/Pages/Portfolio/Portfolio.jsx
+++ src/Pages/Portfolio/Portfolio.jsx
@@ -1,13 +1,27 @@
 import ProjectCard from "../../Components/ProjectCard";
 import useProject from "../../Hooks/useProject";
 import Footer from '../../Components/Footer';
+import { motion } from "framer-motion";
 
 const Portfolio = () => {
     const [projects] = useProject();
     return (
         <div className=" max-w-screen-xl mx-auto">
             <div className="py-16">
-                <h1 className="text-4xl md:text-6xl text-center font-bold py-8 text-transparent bg-clip-text bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500">Portfolio</h1>
+            <div>
+                <motion.h1
+                    initial={{ x: -200, opacity: 0 }}
+                    whileInView={{ x: 0, opacity: 1 }}
+                    transition={{
+                        delay: 0.9,
+                        x: { type: "spring", stiffness: 30 },
+                        opacity: { duration: 2 },
+                        ease: "easeIn",
+                        duration: 1,
+                    }}
+                    className="text-4xl md:text-5xl font-bold text-center border-b-4 max-w-[350px] p-3 mx-auto text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-pink-500">Portfolio</motion.h1>
+
+            </div>
 
                 {
                     projects.map((project, index) => (
